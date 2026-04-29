b68e84c65436b4f357bd2fba673e4af576e27583
Daniela Fonseca <daniela.fonsecazumbado@gmail.com>
2026-04-29 17:31:41 -0600

    Subir primera versión del programa

---
+++ Tarea_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    //int cantidadNumeros;
+    //int contador;
+
+    static int obtenerNumero() {
+        int numero = int.Parse(Console.ReadLine());
+        return numero;
+    }
+    
+    static void Main() {
+        Console.WriteLine("¿Cuántos números desea ingresar? ");
+        int cantidadNumeros = obtenerNumero();
+    }
+}
