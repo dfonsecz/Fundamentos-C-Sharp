486d044f8533fca46d99f87069eddacad31e2fd6
Daniela Fonseca <daniela.fonsecazumbado@gmail.com>
2026-04-29 17:43:45 -0600

    Add method to clasify input number

--- Tarea_1/Program.cs
+++ Tarea_1/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 
 class Program {
-    //int cantidadNumeros;
-    //int contador;
+    static int cantidadPositivos = 0;
+    static int cantidadNegativos = 0;
+    static int cantidadCeros = 0;
 
     static int obtenerNumero() {
         int numero = int.Parse(Console.ReadLine());
         return numero;
     }
+
+    static void clasificarNumero(int numero) {
+        if (numero > 0) {
+            cantidadPositivos++;
+        }
+        else if (numero < 0) {
+            cantidadNegativos++;
+        }
+        else {
+            cantidadCeros++;
+        }
+    }
     
     static void Main() {
         Console.WriteLine("¿Cuántos números desea ingresar? ");
         int cantidadNumeros = obtenerNumero();
+
+        for (int i = 0; i < cantidadNumeros; i++) {
+            Console.WriteLine("Ingrese el número " + (i + 1) + ": ");
+            int numero = obtenerNumero();
+            clasificarNumero(numero);
+        }
     }
 }
